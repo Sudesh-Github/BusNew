aefca030afc2d26db421eddc1277b43f02938266
Sudesh K <Sudesh@SudeshK.com>
2022-05-01 22:44:39 +0530

    Add project files.

---
+++ Models/BusDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BusNew.Models
+{
+    public class BusDbContext : DbContext
+    {
+        public BusDbContext()
+            : base("name=conn")
+        {
+
+        }
+
+        public DbSet<Bus> Buses { get; set; }
+    }
+}
